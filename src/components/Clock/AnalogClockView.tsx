fbc9bcc49e62c1bd524c1f77279520673d17fa02
emilisdigital <emilisdigital@gmail.com>
2023-01-31 15:27:50 +0400

    change clock

--- src/components/Clock/AnalogClockView.tsx
+++ src/components/Clock/AnalogClockView.tsx
@@ -4,20 +4,20 @@ import style from './AnalogClockView.module.css'
 export const AnalogClockView: React.FC<ClockViewPropsType> = ({ date }) => {
 
     const secondsStyle = {
-        transform: `rotate(${date.getSeconds() * 6} deg)`
+        transform: `rotate(${date.getSeconds() * 6}deg)`
     }
     const minutesStyle = {
-        transform: `rotate(${date.getMinutes() * 6} deg)`
+        transform: `rotate(${date.getMinutes() * 6}deg)`
     }
     const hoursStyle = {
-        transform: `rotate(${date.getHours() * 30} deg)`
+        transform: `rotate(${date.getHours() * 30}deg)`
     }
 
     return <>
         <div className={style.clock}>
             <div>
-                <div className={style.info+" "+style.date}></div>
-                <div className={style.info+" "+style.date}></div>
+                <div className={style.info + " " + style.date}></div>
+                <div className={style.info + " " + style.date}></div>
             </div>
             <div className={style.dot}></div>
             <div>
@@ -26,10 +26,10 @@ export const AnalogClockView: React.FC<ClockViewPropsType> = ({ date }) => {
                 <div className={style.secondHand} style={hoursStyle}></div>
             </div>
             <div>
-                <span className={style.h3+" "+style.spanTest}>3</span>
-                <span className={style.h6+" "+style.spanTest}>6</span>
-                <span className={style.h9+" "+style.spanTest}>9</span>
-                <span className={style.h12+" "+style.spanTest}>12</span>
+                <span className={style.h3 + " " + style.spanTest}>3</span>
+                <span className={style.h6 + " " + style.spanTest}>6</span>
+                <span className={style.h9 + " " + style.spanTest}>9</span>
+                <span className={style.h12 + " " + style.spanTest}>12</span>
             </div>
             <div className={style.diallins}></div>
         </div>
